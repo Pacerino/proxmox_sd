f3a9e763c2ab53119c1da19cc9654abf7afcefb8
Pac <pac@pa0x43.me>
2022-02-18 19:03:31 +0100

    Fixed build command

--- .drone.sh
+++ .drone.sh
@@ -6,13 +6,13 @@ set -x
 export CGO_ENABLED=0
 
 # compile for all architectures
-GOOS=linux   GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/amd64/proxmoxsd       ./proxmoxsd
-GOOS=linux   GOARCH=arm64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm64/proxmoxsd       ./proxmoxsd
-GOOS=linux   GOARCH=ppc64le go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/ppc64le/proxmoxsd       ./proxmoxsd
-GOOS=linux   GOARCH=arm     go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm/proxmoxsd         ./proxmoxsd
-GOOS=windows GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/windows/amd64/proxmoxsd.exe ./proxmoxsd
-GOOS=darwin  GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/amd64/proxmoxsd      ./proxmoxsd
-GOOS=darwin  GOARCH=arm64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/arm64/proxmoxsd      ./proxmoxsd
+GOOS=linux   GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/amd64/proxmoxsd .
+GOOS=linux   GOARCH=arm64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm64/proxmoxsd .
+GOOS=linux   GOARCH=ppc64le go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/ppc64le/proxmoxsd .
+GOOS=linux   GOARCH=arm     go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/linux/arm/proxmoxsd .
+GOOS=windows GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/windows/amd64/proxmoxsd.exe .
+GOOS=darwin  GOARCH=amd64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/amd64/proxmoxsd .
+GOOS=darwin  GOARCH=arm64   go build -ldflags "-X main.version=${DRONE_TAG##v}" -o release/darwin/arm64/proxmoxsd .
 
 # tar binary files prior to upload
 tar -cvzf release/proxmoxsd_linux_amd64.tar.gz   -C release/linux/amd64   proxmoxsd
